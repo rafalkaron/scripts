5191d0828aa2ffb9f23404d31748c314d30b0244
Rafał Karoń <rafalkaron@gmail.com>
2022-02-20 12:49:05 +0100

    Added deja-dup follow-up

--- fedora-backup.sh
+++ fedora-backup.sh
@@ -13,18 +13,26 @@ local_snapshots_number=$(find $local_snapshots_dir -maxdepth 1 -type d | wc -l)
 remote_snapshots_number=$(find $remote_snapshots_dir -maxdepth 1 -type d | wc -l)
 
 # Create a snapshot, try to send the snapshot progressively, fallback to non-progressive send
-sudo btrfs subvolume snapshot -r / $local_snapshots_dir/$new_backup && sudo btrfs send -p $local_snapshots_dir/$previous_backup $local_snapshots_dir/$new_backup | sudo btrfs receive $remote_snapshots_dir || sudo btrfs send $local_snapshots_dir/$new_backup | sudo btrfs receive $remote_snapshots_dir
 
-# Delete local snapshots if their number exceeeds 3
-if (($local_snapshots_number > 3));
+if [[ "$new_backup" != "$previous_backup" ]];
 then
-  sudo btrfs subvolume delete $local_snapshots_dir/$oldest_backup
-fi
+  echo "Creating and sending a full system snapshot"
+  sudo btrfs subvolume snapshot -r / $local_snapshots_dir/$new_backup && sudo btrfs send -p $local_snapshots_dir/$previous_backup $local_snapshots_dir/$new_backup | sudo btrfs receive $remote_snapshots_dir || sudo btrfs send $local_snapshots_dir/$new_backup | sudo btrfs receive $remote_snapshots_dir
 
-# Delete remote snapshots if their number exceeds 3
-if (($remote_snapshots_number > 3));
-then
-  sudo btrfs subvolume delete $remote_snapshots_dir/$oldest_backup
+  # Delete local snapshots if their number exceeeds 3
+  if (($local_snapshots_number > 3));
+  then
+    sudo btrfs subvolume delete $local_snapshots_dir/$oldest_backup
+  fi
+
+  # Delete remote snapshots if their number exceeds 3
+  if (($remote_snapshots_number > 3));
+  then
+    sudo btrfs subvolume delete $remote_snapshots_dir/$oldest_backup
+  fi
+
+else
+  echo "A full system snapshot has alreadby been created today"
 fi
 
 # Inform about the results
@@ -32,3 +40,7 @@ echo "Local snapshots:"
 echo $(ls $local_snapshots_dir)
 echo "Remote snapshots:"
 echo $(ls $remote_snapshots_dir)
+
+# Deja-dup
+echo "Running an incremental deja-dup backup of ~/"
+deja-dup --backup
