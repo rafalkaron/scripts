8476d36257b9abe2f74d623ff6c586f9ee140460
Rafał Karoń <rafalkaron@gmail.com>
2022-02-20 13:04:54 +0100

    Added offline-updates check

--- fedora-update.sh
+++ fedora-update.sh
@@ -1,3 +1,12 @@
 flatpak update --user
 sudo dnf upgrade
+echo "Checking if a reboot/relog is needed"
 sudo tracer
+sudo dnf offline-upgrade download
+
+read -p "Reboot to install offline updates? [y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo dnf offline-upgrade reboot
+fi
