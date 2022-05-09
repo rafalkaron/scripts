cc7ce3a78dbfa267f76d9fa0bd683385a1da5922
Rafał Karoń <rafalkaron@gmail.com>
2022-05-09 23:12:01 +0200

    Search script functional

--- fedora/search.sh
+++ fedora/search.sh
@@ -1,18 +1,20 @@
 # Display help
-display_help()
-{
-echo "Usage: $0 [packet_name...]" && exit
-}
-
-if [[ $1 = "-h" ]] || [[ $1 = "--help" ]] || [[ -z "$1" ]]; then
-  display_help
+if [[ $1 = "-h" ]] || [[ $1 = "--help" ]] || [[ -z "$1" ]]
+    then
+        echo "Usage: $0 [packet_name...]" && exit
 fi
 
-# Search Flatpack packages
-#flatpak search $1
+echo "[i] Searching for flatpak packages..."
+flatpak search $1
+echo "[i] Checking if the $1 flatpak package is installed..."
+flatpak list | grep $1
 
-# Search DNF packages
-#sudo dnf search $1
+echo "[i] Searching for dnf packages..."
+sudo dnf search $1
+echo "[i] Checking if the $1 dnf package is installed..."
+sudo dnf list | grep $1
 
-# search homebrew packages
-#brew search $1
+echo "[i] Searching for brew packages..."
+brew search $1
+echo "[i] Checking if the $1 brew package is installed..."
+brew list | grep $1
